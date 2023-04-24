13b45e6b380f5ff5c1eb15b574b6cce738213090
Isaias Garcia <isaias@Isaiass-MacBook-Air.local>
2023-04-24 12:07:48 -0700

    pseudocode and code for vowel-consonant.js file have been implemented

---
+++ vowel-vs-consonant.js
@@ -0,0 +1,23 @@
+//Word that needs to be analyzed
+const word = "textbook";
+
+//Array of vowels 
+const vowels = ["a", "e", "i", "o", "u"];
+let vowelCount = 0;
+let consonantCount = 0;
+
+for(let i = 0; i < word.length; i++) {
+  let letter = word[i];
+
+//A string of lowercase letters should be analyze to see how many consonants and how many vowels it has.
+  console.log("inclues()", vowels.includes(letter))
+  if(vowels.includes(letter)) {
+    vowelCount++;
+  } else {
+    consonantCount++;
+  }
+}
+
+
+// The string should  be printed alongside the number of consonants and vowels.
+console.log(`${word} has ${consonantCount} consonants and ${vowelCount} vowels`)
