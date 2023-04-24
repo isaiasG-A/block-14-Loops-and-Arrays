3c036ec9d831cfb3137e94c97298c2c0065b3d19
Isaias Garcia <isaias@Isaiass-MacBook-Air.local>
2023-04-24 12:19:16 -0700

    more pseudocode has been added to better explain logic behind each of the steps

--- only-odds.js
+++ only-odds.js
@@ -1,12 +1,20 @@
 //A set of numbers will analyzed and a new set will be returned containing only the odd numbers from the first set.
 
+//Set of numbers that will be iterated
 const nums = [2, 4, 6, 8, 10, 11, 12];
+
+//This new set will only contain odd numbers
 const odds = [];
 
 
+//Iteration of nums set
 for(let i = 0; i < nums.length; i++) {
+  //each individual number will be analyzed
   let num = nums[i];
+  
+  //Condition will check if a remainder exists when a number is divided by 2. If it does, then the number is an odd number
   if(num % 2 != 0) {
+//Each number that does have a remainder will be added to "odds" array.
     odds.push(num);
   }
 }
---
+++ reverse-array.js
@@ -0,0 +1,9 @@
+//set of numbers that needs a reverse order.
+const setNums = [1, 3, 5, 7, 9, 11];
+
+// A set of numbers needs to be analyzed and the order of the set needs to be reversed afterwards.
+
+//If a set of numbers looks like this: [1, 3, 5, 7, 9, 11], then the expected output should look like this: [ 11, 9, 7, 5, 3, 1 ]
+
+//output
+console.log(setNums.reverse());
